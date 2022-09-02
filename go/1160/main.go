f75c8ed3d93d494be24e5f3c20c5f4fcbfabd92b
Lars <Nielsen>
2022-09-02 22:22:12 +0200

    1160: works on my machine but not on leetcode

---
+++ go/1160/main.go
@@ -0,0 +1,45 @@
+package main
+
+import "fmt"
+import "strings"
+
+func countCharacters(words []string, chars string) int {
+	count := 0;
+	
+	for _, word := range words {
+
+		accepted := true;
+		
+		for _, chr := range word {
+			if !strings.Contains(chars, string(chr)) {
+				accepted = false;
+				break;
+			}
+		}
+
+		if accepted {
+			count = count + len(word);
+		}
+	}
+	return count;
+}
+
+func main() {
+
+	words := []string{"cat","bt","hat","tree"};
+	chars := "atach";	
+	fmt.Println(countCharacters(words, chars));
+
+	words = []string{"hello","world","leetcode"};
+	chars = "welldonehoneyr";
+	fmt.Println(countCharacters(words, chars));
+
+	words = []string{"dyiclysmffuhibgfvapygkorkqllqlvokosagyelotobicwcmebnpznjbirzrzsrtzjxhsfpiwyfhzyonmuabtlwin","ndqeyhhcquplmznwslewjzuyfgklssvkqxmqjpwhrshycmvrb","ulrrbpspyudncdlbkxkrqpivfftrggemkpyjl","boygirdlggnh","xmqohbyqwagkjzpyawsydmdaattthmuvjbzwpyopyafphx","nulvimegcsiwvhwuiyednoxpugfeimnnyeoczuzxgxbqjvegcxeqnjbwnbvowastqhojepisusvsidhqmszbrnynkyop","hiefuovybkpgzygprmndrkyspoiyapdwkxebgsmodhzpx","juldqdzeskpffaoqcyyxiqqowsalqumddcufhouhrskozhlmobiwzxnhdkidr","lnnvsdcrvzfmrvurucrzlfyigcycffpiuoo","oxgaskztzroxuntiwlfyufddl","tfspedteabxatkaypitjfkhkkigdwdkctqbczcugripkgcyfezpuklfqfcsccboarbfbjfrkxp","qnagrpfzlyrouolqquytwnwnsqnmuzphne","eeilfdaookieawrrbvtnqfzcricvhpiv","sisvsjzyrbdsjcwwygdnxcjhzhsxhpceqz","yhouqhjevqxtecomahbwoptzlkyvjexhzcbccusbjjdgcfzlkoqwiwue","hwxxighzvceaplsycajkhynkhzkwkouszwaiuzqcleyflqrxgjsvlegvupzqijbornbfwpefhxekgpuvgiyeudhncv","cpwcjwgbcquirnsazumgjjcltitmeyfaudbnbqhflvecjsupjmgwfbjo","teyygdmmyadppuopvqdodaczob","qaeowuwqsqffvibrtxnjnzvzuuonrkwpysyxvkijemmpdmtnqxwekbpfzs","qqxpxpmemkldghbmbyxpkwgkaykaerhmwwjonrhcsubchs"}
+
+	chars = "usdruypficfbpfbivlrhutcgvyjenlxzeovdyjtgvvfdjzcmikjraspdfp";
+	fmt.Println(countCharacters(words, chars));
+	
+	
+
+	
+}
