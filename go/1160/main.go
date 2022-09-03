d735107d84be0f951c28571a20240e8ecca9a3a4
Lars <Nielsen>
2022-09-03 08:40:48 +0200

    1160: fixing errors in code workin now

--- go/1160/main.go
+++ go/1160/main.go
@@ -1,20 +1,42 @@
 package main
 
 import "fmt"
-import "strings"
+
+func frequencyMap(chars string) map[rune]int {
+
+	frequency := make(map[rune]int)
+	
+	for _, elm := range chars {
+		value, exists := frequency[elm]
+
+		if exists {
+			frequency[elm] = value + 1
+		} else {
+			frequency[elm] = 1
+		}
+	}
+
+	return frequency 
+}
 
 func countCharacters(words []string, chars string) int {
 	count := 0;
+
+	charsFrequencyMap := frequencyMap(chars)
 	
 	for _, word := range words {
 
+		wordFrequencyMap := frequencyMap(word)
+		
 		accepted := true;
 		
 		for _, chr := range word {
-			if !strings.Contains(chars, string(chr)) {
-				accepted = false;
-				break;
-			}
+			_, exists := charsFrequencyMap[chr]
+
+			if !exists || (exists && charsFrequencyMap[chr] < wordFrequencyMap[chr]) {
+				accepted = false
+				break;				
+			} 
 		}
 
 		if accepted {
