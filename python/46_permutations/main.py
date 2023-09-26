63d9a06d4dd40cbbb95ef52cbf551faee7c22836
Lars <Nielsen>
2023-09-26 20:08:00 +0200

    [:hammer:] Started using python

---
+++ python/46_permutations/main.py
@@ -0,0 +1,15 @@
+class Solution:
+    def permute(self, nums: List[int]) -> List[List[int]]:
+        if len(nums) == 1:
+            return [nums]
+
+        result = list()
+
+        for i in range(0, len(list)):
+
+            
+
+
+
+
+
