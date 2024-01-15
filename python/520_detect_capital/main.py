1c516c0964c3aa71d761c40ae070cf4a9bb4d0b2
Lars Nielsen <lnc13.lars@gmail.com>
2024-01-15 20:07:53 +0100

    [:hammer:] cleaner solution

--- python/520_detect_capital/main.py
+++ python/520_detect_capital/main.py
@@ -3,10 +3,10 @@ import unittest
 class Solution:
     def detectCapitalUse(self, word: str) -> bool:
 
-        first = self.__is_capital(ord(word[0]))
+        first = ord(word[0]) >= 65 and ord(word[0]) <= 90
         number_of_caps = 0 
         for i in range(0, len(word)):
-            if self.__is_capital(ord(word[i])):
+            if ord(word[i]) >= 65 and ord(word[i]) <= 90:
                 number_of_caps = number_of_caps + 1
         return number_of_caps == 0 or (number_of_caps == 1 and first) or number_of_caps == len(word)
             
